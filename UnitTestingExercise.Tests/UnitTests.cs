6eba54c8c176be6e4e57bd5837855c910a42923f
Kiraandaja <kiraandaja@gmail.com>
2022-05-30 15:58:57 -0700

    initial commit

--- UnitTestingExercise.Tests/UnitTests.cs
+++ UnitTestingExercise.Tests/UnitTests.cs
@@ -13,55 +13,91 @@ namespace UnitTestingExercise.Tests
 
             //Arrange
             // create a Calculator object
-            
+
+            var test = new UnitTests();
+
 
             //Act
-                // call the Add method that is located in the Calculator class
-                // and store its result in a variable named actual
+            // call the Add method that is located in the Calculator class
+            // and store its result in a variable named actual
+
+            var actual = test.Add(num1, num2, num3);
 
             //Assert
-                //Assert.Equal(expected, actual);
+            //Assert.Equal(expected, actual);
+            Assert.Equal(expected, actual);
+        }
+
+        private object Add(int num1, int num2, int num3)
+        {
+            throw new NotImplementedException();
         }
 
         [Theory]
-        [InlineData()]//Add test data <-------
+        [InlineData(5,5,0)]//Add test data <-------
         public void SubtractTest(int minuend, int subtrhend, int expected)
         {
             //Start Step 5 here:
 
             //Arrange
+            var tester = new UnitTests();
 
             //Act
+            var actual = tester.Subtract( minuend,  subtrhend);
 
             //Assert
-
+            Assert.Equal(expected, actual);
         }
 
-        [Theory]
-        [InlineData()]//Add test data <-------
-        public void MultiplyTest(int num1, int num2, int expected)
+        private object Subtract(int minuend, int subtrhend)
         {
-            //Start Step 7 here:
+            throw new NotImplementedException();
+        }
 
-            //Arrange
-
-            //Act
-
-            //Assert
-
+        internal object Multiply(int num1, int num2)
+        {
+            throw new NotImplementedException();
         }
 
         [Theory]
-        [InlineData()]//Add test data <-------
+        [InlineData(20, 4, 5)]//Add test data <-------
         public void DivideTest(int num1, int num2, int expected)
         {
             //Arrange
+            var tester = new UnitTests();
 
             //Act
+            var actual = tester.Divide(num1, num2);
 
             //Assert
-
+            Assert.Equal(expected, actual);
         }
 
+        private object Divide(int num1, int num2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+        [Theory]
+        [InlineData(3,4,12)]//Add test data <-------
+        public void MultiplyTest(int num1, int num2, int expected)
+        {
+        //Arrange
+        var tester = new UnitTests();
+
+        //Act
+        var actual = tester.Multiply(num1, num2);
+
+        //Assert
+        Assert.Equal(expected, actual);
     }
 }
+       
+
+
+
+
+    
+
+
